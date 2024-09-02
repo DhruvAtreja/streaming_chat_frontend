4dd64c9c9573342098d7c9ca8f91575df99dc8c7
bracesproul <braceasproul@gmail.com>
2024-09-02 16:25:34 -0700

    support interrupts

--- src/utils/chatApi.ts
+++ src/utils/chatApi.ts
@@ -1,3 +1,5 @@
+import { ThreadState } from "@langchain/langgraph-sdk";
+
 export const createAssistant = async (graphId: string) => {
   const response = await fetch("/api/createAssistant", {
     method: "POST",
@@ -11,10 +13,31 @@ export const createThread = async () => {
   return response.json();
 };
 
+export const getThreadState = async (
+  threadId: string
+): Promise<ThreadState<Record<string, any>>> => {
+  const response = await fetch("/api/getThreadState", {
+    method: "POST",
+    body: JSON.stringify({ threadId }),
+  });
+  return response.json();
+};
+
+export const updateState = async (
+  threadId: string,
+  newState: Record<string, any>
+): Promise<ThreadState<Record<string, any>>> => {
+  const response = await fetch("/api/updateState", {
+    method: "POST",
+    body: JSON.stringify({ threadId, newState }),
+  });
+  return response.json();
+};
+
 export const sendMessage = async (params: {
   threadId: string;
   assistantId: string;
-  message: string;
+  message: string | null;
   model: string;
   userId: string;
   systemInstructions: string;
